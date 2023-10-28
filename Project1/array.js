e46df0c97f93e0ab060b23ee28a7b4a987524b9f
Md Shahnewaj Sheikh <shahanewajshaikh786@gmail.com>
2023-10-28 23:40:57 +0600

    array in js

---
+++ Project1/array.js
@@ -0,0 +1,2 @@
+const item = [10,15,20,25] // Declear array in js
+console.log(item);
--- Project1/index.js
+++ Project1/index.js
@@ -1,34 +1,33 @@
-var x = "Hello World"; // when declear any variable try to avoid "var". "var" is not optimal for declear variable. it wont show any proper error for multiple declearation. Best practice is to use const or let
+var x = "Hello World"; 
 
 console.log (x);
 
-let a = 12; // in node js no need to declear data type of variable. It will take the data type itself. 
+let a = 12; 
 const b = 13;
 let c = "1";
-let d = b+c; // add a string and a number
+let d = b+c; 
 
 
 console.log (a+b);
 
-console.log (d); // as we know output will be string
+console.log (d); 
 
 console.log (typeof (a));
 
-console.log (typeof (a+b)); // for check data type of a variable use typeof()
-
+console.log (typeof (a+b)); 
 console.log (typeof d);
 
 let z = 2;
-z = undefined; // When need to earase or delete any variable 
+z = undefined; 
 console.log(z);
 
 let p = "new"
-p = null; // When need to earase or delete any variable 
+p = null; 
 console.log(p);
 
 
-let o = { // let a = {} this how an object is defined in js
-    name: "Devid jack", // a: value, b: value --> this is way of define key or property for object
+let o = { 
+    name: "Devid jack", 
     age: 30,
     number: "+880111111"
 }

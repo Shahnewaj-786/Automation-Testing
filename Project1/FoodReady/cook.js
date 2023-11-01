061a97cc949ebd111277b6953cdbe04b11b209e5
Md Shahnewaj Sheikh <shahanewajshaikh786@gmail.com>
2023-11-02 01:25:48 +0600

    Creat a time line for promise method

---
+++ Project1/FoodReady/cook.js
@@ -0,0 +1,9 @@
+/**
+ * Expot a class --> declear as a variable with class name = required ("./FolderName/fileName");
+ * Next step to creat an objetc for that class
+ */
+
+
+const FoodReady = required ('./FoodReady/task'); //Np need to wride extension after file name as JS auto detect this. 
+
+const foodCook = new FoodReady();

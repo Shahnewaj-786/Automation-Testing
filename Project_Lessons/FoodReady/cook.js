53ab6640d826b0a23e69cddd536e0e17df038776
Shahnewaj Sheikh <63872577+Shahnewaj-786@users.noreply.github.com>
2023-11-03 10:12:01 +0600

    Delete Project_Lessons directory

--- Project_Lessons/FoodReady/cook.js
+++
@@ -1,9 +0,0 @@
-/**
- * Expot a class --> declear as a variable with class name = required ("./FolderName/fileName");
- * Next step to creat an objetc for that class
- */
-
-
-const FoodReady = required ('./FoodReady/task'); //No need to write extension after file name as JS auto detect this. 
-
-const foodCook = new FoodReady();

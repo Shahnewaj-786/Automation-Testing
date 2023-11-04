3fad10c12edc181bcf410cfcd2058d46aa56467c
Md Shahnewaj Sheikh <shahanewajshaikh786@gmail.com>
2023-11-05 00:29:09 +0600

    Assertion and test with mocha

---
+++ Project1/Automation with Mocha and Chai/calculation.test.js
@@ -0,0 +1,27 @@
+// description: Sumation os 2 numbers
+// execution: expected = 6, actual = 5
+// assertion: true e=a, false e != a 
+
+const {expect} = require ('chai');
+
+describe ("Calculation testing", function (){
+    it ("Sum of two numbers", function(){
+
+        let num1 = 10;
+        let num2 = 20;
+        let expectedValue = 30;
+        let actualValue = num1+num2;
+
+        expect(expectedValue).equal(actualValue);
+    });
+
+    it ("Sub of two numbers", function(){
+
+        let num1 = 10;
+        let num2 = 20;
+        let expectedValue = 300;
+        let actualValue = num1-num2;
+
+        expect(expectedValue).equal(actualValue);
+    });
+});
